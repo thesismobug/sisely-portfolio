90c12df3ea642d505ad53f307039d0144387d04a
francisco-the-man <avery@louisfamily.com>
2025-04-04 10:10:07 -0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import Head from 'next/head';
-import Navbar from '../components/Navbar'; // Import the Navbar component
-import styles from '../styles/Home.module.css'; // Import the CSS module
+import Navbar from '../components/Navbar'; // Import the Navbar
+import styles from '../styles/Home.module.css'; // Import styles for this page
+
 
 const HomePage = () => {
   return (
-    <div className={styles.page}> {/* Add this className */}
+    <div>
       <Head>
-        <title>Sisely Portfolio</title> {/* Update title */}
-        <meta name="description" content="Welcome to Sisely's Portfolio" />
-        
+        <title>Clothing - Sisely Portfolio</title>
+        <meta name="description" content="Sisely's clothing designs" />
       </Head>
 
-      {/* Render the Navbar */}
       <Navbar />
 
-      {/* Main content of the page */}
       <main className={styles.main}> {/* Change from mainContent to main */}
         <p>Hi, I&apos;m Sisely.</p>
         <p>I&apos;m an artist and a student at Stanford.</p>
         <p>You can contact me at delisi [at] stanford [dot] edu</p>
       </main>
-
     </div>
   );
 };
-
 
 export default HomePage; 
