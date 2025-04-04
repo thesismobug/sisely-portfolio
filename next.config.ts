032cf476219d4381ebffbd44580194ac8cc55848
francisco-the-man <avery@louisfamily.com>
2025-04-03 18:37:23 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,30 @@
-import type { NextConfig } from "next";
+    /** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+    const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-export default nextConfig;
+    let assetPrefix = ''
+    let basePath = ''
+
+    if (isGithubActions) {
+      const repoOwner = 'thesismobug'
+      const repoName = 'sisely-portfolio'
+      assetPrefix = `/${repoName}/`
+      basePath = `/${repoName}`
+    }
+
+    const nextConfig = {
+      reactStrictMode: true,
+      assetPrefix: assetPrefix,
+      basePath: basePath,
+      images: {
+        unoptimized: true,
+      },
+      output: 'export',
+      distDir: 'out',
+      env: {
+        NEXT_PUBLIC_BASE_PATH: basePath,
+      },
+      trailingSlash: true,
+    }
+
+    module.exports = nextConfig
