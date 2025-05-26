1ccd932f6b08c8148f0518b5360b0d517a44b590
francisco-the-man <avery@louisfamily.com>
2025-05-26 11:45:28 -0700

    adding new paintings

--- src/pages/painting.js
+++ src/pages/painting.js
@@ -6,19 +6,25 @@ import styles from '../styles/Painting.module.css'; // Import styles for this pa
 
 
 const paintings = [
-  { id: 1, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/1.png`, alt: 'Lucien Freud mastercopy', caption: '16 x 20 in. - oil on canvas' },
-  { id: 2, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/2.png`, alt: 'Salman Toor mastercopy', caption: '16 x 20 in. - oil on canvas' },
-  { id: 3, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/3.png`, alt: 'Avery portrait', caption: '16 x 20 in. - oil on canvas' },
-  { id: 4, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/4.png`, alt: 'Dominic & Doodles portrait', caption: '8 x 10 in. - oil on canvas', row: 2 },
-  { id: 5, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/5.png`, alt: 'Large red self-portrait', caption: '40 x 60 in. - oil on canvas', row: 3 },
-  { id: 6, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/Doni.png`, alt: 'Doni portrait', caption: '8 x 10in. - oil on canvas', row: 2 },
+  { id: 1, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/1.png`, alt: 'Lucien Freud mastercopy', caption: '16 x 20 in. - oil on canvas', row: 3},
+  { id: 2, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/2.png`, alt: 'Salman Toor mastercopy', caption: '16 x 20 in. - oil on canvas', row:3},
+  { id: 3, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/3.png`, alt: 'Avery portrait', caption: '16 x 20 in. - oil on canvas', row:3},
+  { id: 4, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/4.png`, alt: 'Dominic & Doodles portrait', caption: '8 x 10 in. - oil on canvas', row: 4 },
+  { id: 5, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/5.png`, alt: 'Large red self-portrait', caption: '40 x 60 in. - oil on canvas', row: 5 },
+  { id: 6, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/Doni.png`, alt: 'Doni portrait', caption: '8 x 10 in. - oil on canvas', row: 4 },
+  { id: 7, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/sterlyn.png`, alt: 'Sterlyn portrait', caption: '16 x 20 in. - oil on canvas', row: 2 },
+  { id: 8, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/indigo.png`, alt: 'Indigo Menace', caption: '24 x 36 in. - oil on canvas', row: 1 },
+  { id: 9, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/indigo_close.png`, alt: 'Indigo Menace close up', caption: 'close up', row: 1 },
+  { id: 10, src: `${process.env.NEXT_PUBLIC_BASE_PATH}/images/paintings/self_portrait.png`, alt: 'Self Portrait', caption: '16 x 20 in. - oil on canvas', row: 2},
 ];
 
 const PaintingPage = () => {
   // Group paintings by row
-  const firstRow = paintings.filter(p => !p.row || p.row === 1);
+  const firstRow = paintings.filter(p => p.row === 1);
   const secondRow = paintings.filter(p => p.row === 2);
   const thirdRow = paintings.filter(p => p.row === 3);
+  const fourthRow = paintings.filter(p => p.row === 4);
+  const fifthRow = paintings.filter(p => p.row === 5);
 
   return (
     <div>
@@ -30,17 +36,18 @@ const PaintingPage = () => {
       <Navbar />
 
       <main className={styles.pageContainer}>
-        {/* First row - 3 paintings */}
-        <div className={styles.paintingGrid}>
+
+        {/* first row - 2 smaller paintings */}
+        <div className={`${styles.paintingGrid} ${styles.secondRowGrid}`}>
           {firstRow.map((painting) => (
-            <div key={painting.id} className={styles.gridItem}>
+            <div key={painting.id} className={`${styles.gridItem} ${styles.smallerGridItem}`}>
               <div className={styles.imageWrapper}>
                 <Image
                   src={painting.src}
                   alt={painting.alt}
                   layout="responsive"
-                  width={500}
-                  height={600}
+                  width={400}
+                  height={500}
                   objectFit="contain"
                   unoptimized={true}
                 />
@@ -50,7 +57,8 @@ const PaintingPage = () => {
           ))}
         </div>
 
-        {/* Second row - 2 smaller paintings */}
+
+        {/* second row - 2 smaller paintings */}
         <div className={`${styles.paintingGrid} ${styles.secondRowGrid}`}>
           {secondRow.map((painting) => (
             <div key={painting.id} className={`${styles.gridItem} ${styles.smallerGridItem}`}>
@@ -70,9 +78,54 @@ const PaintingPage = () => {
           ))}
         </div>
 
-        {/* Third row - large painting */}
+
+
+        {/* Third row - 3 paintings */}
         <div className={styles.paintingGrid}>
           {thirdRow.map((painting) => (
+            <div key={painting.id} className={styles.gridItem}>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={painting.src}
+                  alt={painting.alt}
+                  layout="responsive"
+                  width={500}
+                  height={600}
+                  objectFit="contain"
+                  unoptimized={true}
+                />
+              </div>
+              <p className={styles.caption}>{painting.caption}</p>
+            </div>
+          ))}
+        </div>
+
+
+        {/* Fourth row - 2 smaller paintings */}
+        <div className={`${styles.paintingGrid} ${styles.secondRowGrid}`}>
+          {fourthRow.map((painting) => (
+            <div key={painting.id} className={`${styles.gridItem} ${styles.smallerGridItem}`}>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={painting.src}
+                  alt={painting.alt}
+                  layout="responsive"
+                  width={400}
+                  height={500}
+                  objectFit="contain"
+                  unoptimized={true}
+                />
+              </div>
+              <p className={styles.caption}>{painting.caption}</p>
+            </div>
+          ))}
+        </div>
+
+
+
+        {/* Fifth row - large painting */}
+        <div className={styles.paintingGrid}>
+          {fifthRowRow.map((painting) => (
             <div
               key={painting.id}
               className={`${styles.gridItem} ${styles.gridItemLarge}`}
